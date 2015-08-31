53c98f20bf799ee53cf6978a06784b50827f761e
Felix J. Ogris <fjo@core7.intra.ogris.net>
2015-09-01 01:24:07 +0200

    yes yes

--- Makefile
+++ Makefile
@@ -2,12 +2,17 @@ CFLAGS=-W -Wall -O3 -pipe
 LDFLAGS=-s
 LIBS=-lpthread
 
+all:	s3nbd lock_chunk
+
 s3nbd:	s3nbd.c
+	$(CC) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $@ $<
+
+lock_chunk:	lock_chunk.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $@ $<
 
 test:	test.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $@ $<
 
-clean:	; -rm s3nbd test
+clean:	; -rm s3nbd lock_chunk test
 
-.PHONY:	clean
+.PHONY:	all clean
---
+++ lock_chunk.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <err.h>
+
+#define __USE_GNU
+#include <fcntl.h>
+
+int main (int argc, char **argv)
+{
+  int fd;
+  struct flock flk;
+  struct stat st;
+  char buf[16];
+
+  if (argc != 5) errx(1, "usage: lock_chunk <r|w> <start> <end> /path/to/chunk");
+
+  fd = open(argv[4], O_RDWR);
+  if (fd < 0) err(1, "open()");
+
+  flk.l_type = (argv[1][0] == 'r' ? F_RDLCK : F_WRLCK);
+  flk.l_whence = SEEK_SET;
+  flk.l_start = atol(argv[2]);
+  flk.l_len = atol(argv[3]);
+  flk.l_pid = 0;
+
+  if (fcntl(fd, F_OFD_SETLK, &flk) != 0)
+    err(1, "fcntl()");
+
+  if (stat(argv[4], &st) != 0)
+    warn("stat()");
+  else if (st.st_size != 8*1024*1024)
+    warnx("filesize != CHUNKSIZE");
+
+  read(0, buf, sizeof(buf));
+
+  return 0;
+}
